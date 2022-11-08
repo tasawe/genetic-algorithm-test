f276a9d1496f69a4a3052232d01b4256b2b6b0f1
João Watase <watasejoao@gmail.com>
2022-11-08 01:06:51 -0300

    adiciona cores e classes

--- classes.py
+++ classes.py
@@ -1,7 +1,17 @@
 from processing_py import *
+from math import sqrt
 
 class Colors():
+    RED = 0xFFFF0000
+    GREEN = 0xFF00FF00
     BLUE = 0xFF0000FF
+    BLACK = 0xFF000000
+    WHITE = 0xFFFFFFFF # creature
+    YELLOW = 0xFFFFFF00 # creature carries diamond
+    DIAMOND = 0xFF00CCFF
+    WATER = 0xFF4D4DFF
+    FIRE = 0xFFFF6600
+    MANSION = 0xFFCC0099
 
 class Neuron():
     def __init__(self, inputs):
@@ -20,6 +30,10 @@ class Entity():
         self.color = color
     def update(self, app):
         pass
+    def inside(self, x, y):
+        return sqrt((x-self.x)*(x-self.x) + (y-self.y)*(y-self.y)) <= self.radius
+    def collide():
+        pass
     def draw(self, app):
         app.fill(self.color)
         app.ellipse(self.x, self.y, self.radius, self.radius)
@@ -27,4 +41,44 @@ class Entity():
 class Water(Entity):
     def __init__(self, x, y):
         self.radius = 25
-        super().__init__(x, y, self.radius, Colors.BLUE)
+        self.color = Colors.WATER
+        super().__init__(x, y, self.radius, self.color)
+
+class Diamond(Entity):
+    def __init__(self, x, y):
+        self.radius = 20
+        self.color = Colors.DIAMOND
+        super().__init__(x, y, self.radius, self.color)
+
+class Fire(Entity):
+    def __init__(self, x, y):
+        self.radius = 25
+        self.color = Colors.FIRE
+        super().__init__(x, y, self.radius, self.color)
+
+class Mansion(Entity):
+    def __init__(self, app):
+        self.x = app.width/2
+        self.y = app.height/2
+        self.color = Colors.MANSION
+        self.radius = 90
+        self.stroke = 5
+        self.innerRadius = self.radius - self.stroke
+    def draw(self, app):
+        app.fill(self.color)
+        app.ellipse(self.x, self.y, self.radius, self.radius)
+        app.fill(Colors.BLACK)
+        app.ellipse(self.x, self.y, self.innerRadius, self.innerRadius)
+
+class Creature(Entity):
+    def __init__(self, x, y):
+        self.radius = 30
+        self.carriesDiamond = False
+        self.color = Colors.WHITE
+        super().__init__(x, y, self.radius, self.color)
+    def update(self, app):
+        self.color = Colors.YELLOW if self.carriesDiamond else Colors.WHITE
+        if (self.inside(app.mouseX, app.mouseY)):
+            self.carriesDiamond = True
+        else:
+            self.carriesDiamond = False
--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from processing_py import *
 import classes
 
 app = App(600,600)
-entities = []
-entities.append(classes.Water(app.width/2, app.height/2))
+app.entities = []
+app.entities.append(classes.Mansion(app))
+app.entities.append(classes.Creature(app.width/2 + 50, app.height/2 + 50))
+app.entities.append(classes.Water(app.width/2, app.height/2))
+app.entities.append(classes.Diamond(app.width/2 + 50, app.height/2))
+app.entities.append(classes.Fire(app.width/2, app.height/2 + 50))
+app.ellipseMode('RADIUS')
 
 def draw():
     app.background(0,0,0)
-    for e in entities:
+    for e in app.entities:
         e.update(app)
         e.draw(app)
     app.redraw()
